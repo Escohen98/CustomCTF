9f16c27b66c6f54a3e747d332573fe94412c0001
Eric C <escohen98@gmail.com>
2020-12-28 16:26:30 -0500

    Added home screen, label, button, and button command

--- client/login_client.py
+++ client/login_client.py
@@ -3,30 +3,35 @@ from connection import connection
 
 #Login Client
 
-#Connect to server
-conn = connection()
-s = conn.connect(12345)
+def main(port):
+    #Connect to server
+    conn = connection()
+    s = conn.connect(port)
 
-success = False;
-s.sendall('login'.encode('utf-8')) 
-while not success: #Loops through until successful login
-    
-    user = input("Username: ")
-    password = input("Password: ")
-    if not user or not password: #Fail-Safe
-        print("Please enter a username and password.")
-        continue
-    s.sendall(user.encode('utf-8'))
-    s.sendall(password.encode('utf-8'))
-    
-    rec1 = s.recv(1024).decode()
-    rec2 = s.recv(1024).decode()
-    
-    #print(rec2)
-    print(rec1) #Flag - Encrypted in rot-13
-    success = (int(rec2) == 1)
-    
- 
-conn.disconnect(s)
+    success = False;
+    s.sendall('login'.encode('utf-8')) 
+    while not success: #Loops through until successful login
+        
+        user = input("Username: ")
+        password = input("Password: ")
+        if not user or not password: #Fail-Safe
+            print("Please enter a username and password.")
+            continue
+        s.sendall(user.encode('utf-8'))
+        s.sendall(password.encode('utf-8'))
+        
+        rec1 = s.recv(1024).decode()
+        rec2 = s.recv(1024).decode()
+        
+        #print(rec2)
+        print(rec1) #Flag - Encrypted in rot-13
+        success = (int(rec2) == 1)
+        
+     
+    conn.disconnect(s)
 
+
+def run(port):
+    main(port)
+run(12345)
 #cd Documents\Python\CustomCTF\client
--- gui/start.py
+++ gui/start.py
@@ -1,16 +1,41 @@
 from tkinter import * 
+import time
 
 window = Tk() #Open window
 window.title('Capture the Flag')
 window.geometry("300x300")
+#To hide buttons
+def hide(events):
+    for event in events:
+        print(type(event))
+        event.destroy()
 
-var = StringVar()
-label = Label(window, textvariable=var, relief=RAISED, height=3)
-start = Button(window, text = "Start")
+#home screen
+def home():
+    var = StringVar()
+    label = Label(window, textvariable=var, relief=RAISED, height=3, font=64)
+    start = Button(window, text = "Start", font=32, padx = 10, command=lambda: hide([start, label]))
+    #start['command'] = hide([start, label])
 
-var.set("Welcome to the CTF!")
-label.pack()
-start.pack()
+    var.set("Welcome to the CTF!")
+    label.pack()
+    start.pack(pady=100)
+
+#Menu Screen    
+def menu():
+    print('Coming Soon')
+    
+def downloadScreen():
+    print('Coming Soon')
+    
+def loginScreen():
+    print('Coming Soon')
+
+def flagScreen():
+    print('Coming Soon')
+
+
+home()
 #bottomframe = Frame(root)
 #bottomframe.pack( side = BOTTOM )
 
