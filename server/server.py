30dee2aafcd001bfa8a1b700239f459ac92289fc
Eric C <escohen98@gmail.com>
2020-12-27 21:54:58 -0500

    Don't know, don't care. It works.

--- server/server.py
+++ server/server.py
@@ -34,7 +34,7 @@ def multi_threaded_client(c):
     while True:
         part = c.recv(1234).decode() #Either send file or login. 
         if (part == 'send'): #Part 1 - Download Files
-            print(os.getcwd)
+            print(os.getcwd())
             #THIS_FOLDER = os.path.dirname(os.path.abspath(__file__)) #Cross-Platform compatibility
             #my_file = os.path.join(THIS_FOLDER, 'tosend.png') 
             file = open('tosend.png', 'rb') #Testing with png. Replace with pcap
@@ -42,10 +42,10 @@ def multi_threaded_client(c):
             bits = file.read(1024)
             while bits: #Sends over file in pieces
                 print('Sending...')
-                s.send(bits)
+                c.sendall(bits)
                 bits = file.read(1024)
             file.close()
-            s.shutdown(socket.SHUT_WR) #Necessary to end buffer
+            c.shutdown(socket.SHUT_WR) #Necessary to end buffer
         elif(part == 'login'): #Part 2 - Input Login Credentials
             auth = False
             while not auth:
@@ -73,13 +73,11 @@ except socket.error as e:
     print(str(e))
 x = 5
 s.listen(x) #waiting for a connection can fail up to x times 
-ThreadCount = 0
+#ThreadCount = 0
 while True:
     c, addr = s.accept()
     print('Got connection from', addr)
     threading.Thread(target=multi_threaded_client, args=(c,  ), daemon=True).start() #Starts the new thread
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
+    #ThreadCount += 1
+    #print('Thread Number: ' + str(ThreadCount))
 s.close()
-    
-    
