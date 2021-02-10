6b09c9ac713518a6048890d4739c33bac53cc110
Eric C <escohen98@gmail.com>
2021-02-09 18:59:15 -0800

    I moved the host to the config file on the client sidegit add -Agit add -Agit add -Agit add -A\!

--- client/connection.py
+++ client/connection.py
@@ -15,7 +15,7 @@ class connection():
         server = socket.socket()
         server.settimeout(5.0)
         #host = "uwischoolreifers.westus2.cloudapp.azure.com" #Might not be a bad idea to put into a config file
-        host = socket.gethostname()
+        host = data["host"]
         print(f"host: {host} type: {type(host)}")
         print(f"port: {port} type: {type(port)}")
         try:
